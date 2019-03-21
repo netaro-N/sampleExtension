94ba8cd7bfd8a4ab7d1dc417b48bdf4407f0c17c
netaro-N <takebbb1+github@gmail.com>
2019-03-21 09:09:18 +0000

    setTimeout

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,11 @@
+let escCounter = 0;
 document.body.onkeydown = function(event) {
   if (event.keyCode === 27) {
+    escCounter += 1;
+    if (escCounter >= 2) {
       location.href = "https://www.nnn.ed.nico/contents/guides/2175/content";
+    } else {
+      setTimeout(function() { escCounter =0; }, 500)
+    }
   }
 }
